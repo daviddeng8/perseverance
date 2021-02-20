acb72060c84cbbe0f64416fe6262311b73a0066e
David Deng <daviddeng8@gmail.com>
2021-02-20 01:17:52 -0800

    added test frontend for user authentication, defined student user model for database

--- server/index.js
+++ server/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const expressEjsLayout = require('express-ejs-layouts');
 
+/*
 app.get('/', function(req, res) {
     res.send("hello, world.");
-})
+})*/
+
+//mongoose connection test 
+mongoose.connect('mongodb://localhost/test',{useNewUrlParser: true, useUnifiedTopology : true})
+.then(() => console.log('connected,,'))
+.catch((err)=> console.log(err));
+
+//EJS
+app.set('view engine','ejs');
+app.use(expressEjsLayout);
+//BodyParser
+app.use(express.urlencoded({extended : false}));
+
+//Routes
+app.use('/',require('./routes/render'));
+app.use('/users',require('./routes/users'));
+
 
 
 
